66f7b623d1d47478bcc7aa6659eb0413f6971948
Arina <aqmarinasha@gmail.com>
2025-02-26 21:39:52 +0700

    optimize controller

--- app/Http/Controllers/SuratController.php
+++ app/Http/Controllers/SuratController.php
@@ -75,268 +75,140 @@ class SuratController extends Controller
     public function store(Request $request)
     {
         $jenis_surat = $request->input('jenis_surat');
+        $user = auth()->user();
+        $rt_rw = $user->rt_rw;
 
-        switch ($jenis_surat) {
-            case 'Surat Pengantar':
-                $rt_rw = auth()->user()->rt_rw;
-                list($rt, $rw) = explode('/', $rt_rw);
+        $validationRules = [
+            'Surat Pengantar' => [
+                'nama' => 'required|string|max:50',
+                'ttl' => 'required|string|max:50|regex:/^[A-Za-z]+, \d{1,2} [A-Za-z]+ \d{4}$/',
+                'jenis_kelamin' => 'required|string|max:15',
+                'agama' => 'required|string|max:15',
+                'pekerjaan' => 'required|string|max:30',
+                'nik' => 'required|digits:16',
+                'keperluan' => 'required|string|max:50',
+                'no_whatsapp' => 'required|digits_between:10,15',
+                'ktp' => 'required|mimes:jpg,jpeg,png|max:1024',
+                'kk' => 'required|mimes:jpg,jpeg,png|max:1024'
+            ],
+            'Surat Domisili' => [
+                'nama' => 'required|string|max:50',
+                'nik' => 'required|digits:16',
+                'ttl' => 'required|string|max:50|regex:/^[A-Za-z]+, \d{1,2} [A-Za-z]+ \d{4}$/',
+                'status_kawin' => 'required|string|max:50',
+                'agama' => 'required|string|max:50',
+                'pekerjaan' => 'required|string|max:30',
+                'alamat' => 'required|string|max:255',
+                'keperluan' => 'required|string|max:50',
+                'no_whatsapp' => 'required|digits_between:10,15',
+                'ktp' => 'required|mimes:jpg,jpeg,png|max:1024',
+                'kk' => 'required|mimes:jpg,jpeg,png|max:1024'
+            ],
+            'Surat Keterangan Tidak Mampu' => [
+                'nama_ortu' => 'required|string|max:50',
+                'nik_ortu' => 'required|digits:16',
+                "ttl_ortu" => 'required|string|max:50|regex:/^[A-Za-z]+, \d{1,2} [A-Za-z]+ \d{4}$/',
+                'jenis_kelamin_ortu' => 'required|string|max:15',
+                'no_whatsapp' => 'required|digits_between:10,15',
+                'status_kawin' => 'required|string|max:20',
+                'alamat' => 'required|string|max:255',
+                'penghasilan' => 'required|string|max:20',
 
-                $ketuaRt = User::where('role', 'rt')
-                    ->where('rt_rw', "$rt_rw")
-                    ->first();
-                $ketuaRw = Rw::where('rw', $rw)->first();
+                'nama' => 'required|string|max:50',
+                'nik' => 'required|digits:16|different:nik_ortu',
+                "ttl" => 'required|string|max:50|regex:/^[A-Za-z]+, \d{1,2} [A-Za-z]+ \d{4}$/|different:ttl_ortu',
+                'jenis_kelamin' => 'required|string|max:15',
+                'sekolah' => 'required|string|max:50',
+                'jurusan' => 'required|string|max:50',
+                'keperluan' => 'required|string|max:50',
 
+                'ktp' => 'required|mimes:jpg,jpeg,png|max:1024',
+                'kk' => 'required|mimes:jpg,jpeg,png|max:1024',
+            ],
+            'Surat Keterangan Usaha' => [
+                'nama' => 'required|string|max:50',
+                'nik' => 'required|digits:16',
+                'ttl' => 'required|string|max:50|regex:/^[A-Za-z]+, \d{1,2} [A-Za-z]+ \d{4}$/',
+                'kewarganegaraan' => 'required|string|max:20',
+                'agama' => 'required|string|max:15',
+                'status_kawin' => 'required|string|max:15',
+                'pekerjaan' => 'required|string|max:30',
+                'alamat' => 'required|string|max:255',
+                'jenis_usaha' => 'required|string|max:50',
+                'no_whatsapp' => 'required|digits_between:10,15',
+                'ktp' => 'required|mimes:jpg,jpeg,png|max:1024',
+                'kk' => 'required|mimes:jpg,jpeg,png|max:1024'
+            ],
+            'Surat Keterangan Belum Menikah' => [
+                'nama' => 'required|string|max:50',
+                'bin' => 'required|string|max:50',
+                'nik' => 'required|digits:16',
+                'ttl' => 'required|string|max:50|regex:/^[A-Za-z]+, \d{1,2} [A-Za-z]+ \d{4}$/',
+                'agama' => 'required|string|max:15',
+                'kewarganegaraan' => 'required|string|max:15',
+                'status_kawin' => 'required|string|max:30',
+                'pekerjaan' => 'required|string|max:30',
+                'alamat' => 'required|string|max:255',
+                'no_whatsapp' => 'required|digits_between:10,15',
+                'ktp' => 'required|mimes:jpg,jpeg,png|max:1024',
+                'kk' => 'required|mimes:jpg,jpeg,png|max:1024'
+            ],
+        ];
 
-                $validateSurat = $request->validate([
-                    'nama' => 'required|string|max:50',
-                    'ttl' => 'required|string|max:50|regex:/^[A-Za-z]+, \d{1,2} [A-Za-z]+ \d{4}$/',
-                    'jenis_kelamin' => 'required|string|max:15',
-                    'agama' => 'required|string|max:15',
-                    'pekerjaan' => 'required|string|max:30',
-                    'nik' => 'required|digits:16',
-                    'keperluan' => 'required|string|max:50',
-                    'no_whatsapp' => 'required|digits_between:10,15',
-                    'ktp' => 'required|mimes:jpg,jpeg,png|max:1024',
-                    'kk' => 'required|mimes:jpg,jpeg,png|max:1024'
-                ]);
-
-                $fileFields = ['ktp', 'kk'];
-                $filePaths = [];
-
-
-                foreach ($fileFields as $field) {
-                    if ($request->hasFile($field)) {
-                        $file = $request->file($field);
-                        $filePaths[$field] = $this->processImageWithWatermark($file, $field);
-                    }
-                }
-
-                $data = array_merge(
-                    $validateSurat,
-                    $filePaths,
-                    [
-                        'rt' => $rt,
-                        'rw' => $rw,
-                        'ketua_rw' => $ketuaRw->nama ?? "-",
-                        'ketua_rt' => $ketuaRt->nama ?? "-",
-                    ]
-                );
-                $suratModel = SuratPengantar::create($data);
-                break;
-            case 'Surat Domisili':
-                $validateSurat = $request->validate([
-                    'nama' => 'required|string|max:50',
-                    'nik' => 'required|digits:16',
-                    'ttl' => 'required|string|max:50|regex:/^[A-Za-z]+, \d{1,2} [A-Za-z]+ \d{4}$/',
-                    'status_kawin' => 'required|string|max:50',
-                    'agama' => 'required|string|max:50',
-                    'pekerjaan' => 'required|string|max:30',
-                    'alamat' => 'required|string|max:255',
-                    'keperluan' => 'required|string|max:50',
-                    'no_whatsapp' => 'required|digits_between:10,15',
-                    'ktp' => 'required|mimes:jpg,jpeg,png|max:1024',
-                    'kk' => 'required|mimes:jpg,jpeg,png|max:1024'
-
-                ]);
-
-                $fileFields = ['ktp', 'kk'];
-                $filePaths = [];
-
-
-                foreach ($fileFields as $field) {
-                    if ($request->hasFile($field)) {
-                        $file = $request->file($field);
-                        $filePaths[$field] = $this->processImageWithWatermark($file, $field);
-                    }
-                }
-
-                $data = array_merge(
-                    $validateSurat,
-                    $filePaths,
-                );
-
-                $suratModel = SuratDomisili::create($data);
-                break;
-            case 'Surat Keterangan Tidak Mampu':
-                $validateSurat = $request->validate([
-                    'nama_ortu' => 'required|string|max:50',
-                    'nik_ortu' => 'required|digits:16',
-                    "ttl_ortu" => 'required|string|max:50|regex:/^[A-Za-z]+, \d{1,2} [A-Za-z]+ \d{4}$/',
-                    'jenis_kelamin_ortu' => 'required|string|max:15',
-                    'no_whatsapp' => 'required|digits_between:10,15',
-                    'status_kawin' => 'required|string|max:20',
-                    'alamat' => 'required|string|max:255',
-                    'penghasilan' => 'required|string|max:20',
-
-                    'nama' => 'required|string|max:50',
-                    'nik' => 'required|digits:16|different:nik_ortu',
-                    "ttl" => 'required|string|max:50|regex:/^[A-Za-z]+, \d{1,2} [A-Za-z]+ \d{4}$/|different:ttl_ortu',
-                    'jenis_kelamin' => 'required|string|max:15',
-                    'sekolah' => 'required|string|max:50',
-                    'jurusan' => 'required|string|max:50',
-                    'keperluan' => 'required|string|max:50',
-
-                    'ktp' => 'required|mimes:jpg,jpeg,png|max:1024',
-                    'kk' => 'required|mimes:jpg,jpeg,png|max:1024',
-                ]);
-
-                $fileFields = ['ktp', 'kk'];
-                $filePaths = [];
-
-
-                foreach ($fileFields as $field) {
-                    if ($request->hasFile($field)) {
-                        $file = $request->file($field);
-                        $filePaths[$field] = $this->processImageWithWatermark($file, $field);
-                    }
-                }
-
-                $data = array_merge(
-                    $validateSurat,
-                    $filePaths,
-                );
-
-                $suratModel = SuratTidakMampu::create($data);
-                break;
-            case 'Surat Keterangan Kematian':
-                $validateSurat = $request->validate([
-                    'nama' => 'required|string|max:50',
-                    'nik' => 'required|digits:16',
-                    'no_whatsapp' => 'required|digits_between:10,15',
-                    'jenis_kelamin' => 'required|string|max:15',
-                    'alamat' => 'required|string|max:255',
-                    'hari_meninggal' => 'required|string|max:30',
-                    'tanggal_meninggal' => 'required|string|max:30|regex:/^\d{1,2} [A-Za-z]+ \d{4}$/',
-                    'tempat_meninggal' => 'required|string|max:30',
-                    'sebab_meninggal' => 'required|string|max:30',
-                    'ktp' => 'required|mimes:jpg,jpeg,png|max:1024',
-                    'kk' => 'required|mimes:jpg,jpeg,png|max:1024',
-                ]);
-
-                $fileFields = ['ktp', 'kk'];
-                $filePaths = [];
-
-
-                foreach ($fileFields as $field) {
-                    if ($request->hasFile($field)) {
-                        $file = $request->file($field);
-                        $filePaths[$field] = $this->processImageWithWatermark($file, $field);
-                    }
-                }
-
-                $data = array_merge(
-                    $validateSurat,
-                    $filePaths,
-                );
-
-                $suratModel = SuratKematian::create($data);
-                break;
-            case 'Surat Keterangan Usaha':
-                $validateSurat = $request->validate([
-                    'nama' => 'required|string|max:50',
-                    'nik' => 'required|digits:16',
-                    'ttl' => 'required|string|max:50|regex:/^[A-Za-z]+, \d{1,2} [A-Za-z]+ \d{4}$/',
-                    'kewarganegaraan' => 'required|string|max:20',
-                    'agama' => 'required|string|max:15',
-                    'status_kawin' => 'required|string|max:15',
-                    'pekerjaan' => 'required|string|max:30',
-                    'alamat' => 'required|string|max:255',
-                    'jenis_usaha' => 'required|string|max:50',
-                    'no_whatsapp' => 'required|digits_between:10,15',
-                    'ktp' => 'required|mimes:jpg,jpeg,png|max:1024',
-                    'kk' => 'required|mimes:jpg,jpeg,png|max:1024'
-                ]);
-
-                $fileFields = ['ktp', 'kk'];
-                $filePaths = [];
-
-
-                foreach ($fileFields as $field) {
-                    if ($request->hasFile($field)) {
-                        $file = $request->file($field);
-                        $filePaths[$field] = $this->processImageWithWatermark($file, $field);
-                    }
-                }
-
-                $data = array_merge(
-                    $validateSurat,
-                    $filePaths,
-                );
-
-                $suratModel = SuratUsaha::create($data);
-                break;
-            case 'Surat Keterangan Belum Menikah':
-                $validateSurat = $request->validate([
-                    'nama' => 'required|string|max:50',
-                    'bin' => 'required|string|max:50',
-                    'nik' => 'required|digits:16',
-                    'ttl' => 'required|string|max:50|regex:/^[A-Za-z]+, \d{1,2} [A-Za-z]+ \d{4}$/',
-                    'agama' => 'required|string|max:15',
-                    'kewarganegaraan' => 'required|string|max:15',
-                    'status_kawin' => 'required|string|max:30',
-                    'pekerjaan' => 'required|string|max:30',
-                    'alamat' => 'required|string|max:255',
-                    'no_whatsapp' => 'required|digits_between:10,15',
-                    'ktp' => 'required|mimes:jpg,jpeg,png|max:1024',
-                    'kk' => 'required|mimes:jpg,jpeg,png|max:1024'
-                ]);
-
-                $fileFields = ['ktp', 'kk'];
-                $filePaths = [];
-
-
-                foreach ($fileFields as $field) {
-                    if ($request->hasFile($field)) {
-                        $file = $request->file($field);
-                        $filePaths[$field] = $this->processImageWithWatermark($file, $field);
-                    }
-                }
-
-                $data = array_merge(
-                    $validateSurat,
-                    $filePaths,
-                );
-
-                $suratModel = SuratBelumNikah::create($data);
-                break;
-                // case 'Surat Keterangan Ahli Waris':
-                //     break;
-                // case 'Surat Keterangan Ahli Waris Bank':
-                //     break;
-                // case 'Surat Pernyataan Kepemilikan Tanah':
-                //     break;
-                // case 'Surat Keterangan Beda Nama':
-                //     break;
-            default:
-                return redirect()->route('surat.index')->with('error', 'Jenis surat tidak valid');
+        if (!isset($validationRules[$jenis_surat])) {
+            return redirect()->route('riwayat.index')->with('error', 'Jenis surat tidak valid.');
         }
 
+        $validateSurat = $request->validate($validationRules[$jenis_surat]);
+
+        $fileFields = ['ktp', 'kk'];
+        $filePaths = [];
+
+
+        foreach ($fileFields as $field) {
+            if ($request->hasFile($field)) {
+                $file = $request->file($field);
+                $filePaths[$field] = $this->processImageWithWatermark($file, $field);
+            }
+        }
+
+        $additionalData = [];
+        if ($jenis_surat === 'Surat Pengantar') {
+            list($rt, $rw) = explode('/', $rt_rw);
+            $ketuaRt = User::where('role', 'rt')->where('rt_rw', $rt_rw)->first();
+            $ketuaRw = Rw::where('rw', $rw)->first();
+
+            $additionalData = [
+                'rt' => $rt,
+                'rw' => $rw,
+                'ketua_rw' => $ketuaRw->nama ?? "-",
+                'ketua_rt' => $ketuaRt->nama ?? "-",
+            ];
+        }
+
+        $modelMap = [
+            'Surat Pengantar' => SuratPengantar::class,
+            'Surat Domisili' => SuratDomisili::class,
+            'Surat Keterangan Tidak Mampu' => SuratTidakMampu::class,
+            'Surat Keterangan Kematian' => SuratKematian::class,
+            'Surat Keterangan Usaha' => SuratUsaha::class,
+            'Surat Keterangan Belum Menikah' => SuratBelumNikah::class,
+        ];
+
+        $modelClass = $modelMap[$jenis_surat];
+        $data = array_merge($validateSurat, $filePaths, $additionalData);
+        $suratModel = $modelClass::create($data);
+
         Surat::create([
-            'user_id' => auth()->id(),
-            'rt_rw' => auth()->user()->rt_rw,
+            'user_id' => $user->id,
+            'rt_rw' => $rt_rw,
             'suratable_type' => get_class($suratModel),
             'suratable_id' => $suratModel->id,
             'jenis_surat' => $jenis_surat,
             'tanggal_pengajuan' => now(),
         ]);
+
         return redirect()->route('riwayat.index')->with('success', 'Berhasil mengajukan ' . $jenis_surat);
-    }
-
-    public function show(string $id) {}
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
     }
 
     /**
--- app/Http/Controllers/VerifSuratController.php
+++ app/Http/Controllers/VerifSuratController.php
@@ -117,27 +117,27 @@ class VerifSuratController extends Controller
         switch ($letter->jenis_surat) {
             case 'Surat Pengantar':
                 $templatePath = public_path('templates/Surat Pengantar RT RW.docx');
-                $outputFilename = 'Surat_Pengantar_' . $detailSurat->id . '.docx';
+                $outputFilename = 'Surat_Pengantar_' . $detailSurat->nama . '.docx';
                 break;
             case 'Surat Keterangan Tidak Mampu':
                 $templatePath = public_path('templates/Surat Keterangan Tidak Mampu.docx');
-                $outputFilename = 'Surat_Tidak_Mampu_' . $detailSurat->id . '.docx';
+                $outputFilename = 'Surat_Tidak_Mampu_' . $detailSurat->nama . '.docx';
                 break;
             case 'Surat Keterangan Kematian':
                 $templatePath = public_path('templates/Surat Keterangan Kematian.docx');
-                $outputFilename = 'Surat_Kematian_' . $detailSurat->id . '.docx';
+                $outputFilename = 'Surat_Kematian_' . $detailSurat->nama . '.docx';
                 break;
             case 'Surat Keterangan Usaha':
                 $templatePath = public_path('templates/Surat Keterangan Usaha.docx');
-                $outputFilename = 'Surat_Usaha_' . $detailSurat->id . '.docx';
+                $outputFilename = 'Surat_Usaha_' . $detailSurat->nama . '.docx';
                 break;
             case 'Surat Keterangan Belum Menikah':
                 $templatePath = public_path('templates/Surat Keterangan Belum Menikah.docx');
-                $outputFilename = 'Surat_Belum_Nikah_' . $detailSurat->id . '.docx';
+                $outputFilename = 'Surat_Belum_Nikah_' . $detailSurat->nama . '.docx';
                 break;
             case 'Surat Domisili':
                 $templatePath = public_path('templates/Surat Keterangan Domisili.docx');
-                $outputFilename = 'Surat_Domisili_' . $detailSurat->id . '.docx';
+                $outputFilename = 'Surat_Domisili_' . $detailSurat->nama . '.docx';
                 break;
             default:
                 return redirect()->route('verifikasi.index')->with('error', 'Template untuk jenis surat ini belum tersedia.');
